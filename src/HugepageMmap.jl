6e77a8e01e90824d32283f2f84fef59e0f7aa879
Mark Hildebrand <mhildebrand@ucdavis.edu>
2020-03-16 11:41:59 -0700

    Files generated by PkgTemplates

---
+++ src/HugepageMmap.jl
@@ -0,0 +1,5 @@
+module HugepageMmap
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using HugepageMmap
+using Test
+
+@testset "HugepageMmap.jl" begin
+    # Write your own tests here.
+end
